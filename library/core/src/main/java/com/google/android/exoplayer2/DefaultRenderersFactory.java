ab9ca3f97e3c36e64fda09ace371df00fb1ec79e
olly <olly@google.com>
2017-08-17 14:20:56 -0700

    Split metadata and text outputs from their renderer classes
    There will be non-ExoPlayer players that can output text
    and metadata, so the outputs should be standalone. There
    may also be ExoPlayer instances that use non-standard
    text and metadata renderers, for which this change also
    makes sense.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=165628420

--- library/core/src/main/java/com/google/android/exoplayer2/DefaultRenderersFactory.java
+++ library/core/src/main/java/com/google/android/exoplayer2/DefaultRenderersFactory.java
@@ -27,7 +27,9 @@ import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer;
 import com.google.android.exoplayer2.drm.DrmSessionManager;
 import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
+import com.google.android.exoplayer2.metadata.MetadataOutput;
 import com.google.android.exoplayer2.metadata.MetadataRenderer;
+import com.google.android.exoplayer2.text.TextOutput;
 import com.google.android.exoplayer2.text.TextRenderer;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.video.MediaCodecVideoRenderer;
@@ -138,7 +140,7 @@ public class DefaultRenderersFactory implements RenderersFactory {
   public Renderer[] createRenderers(Handler eventHandler,
       VideoRendererEventListener videoRendererEventListener,
       AudioRendererEventListener audioRendererEventListener,
-      TextRenderer.Output textRendererOutput, MetadataRenderer.Output metadataRendererOutput) {
+      TextOutput textRendererOutput, MetadataOutput metadataRendererOutput) {
     ArrayList<Renderer> renderersList = new ArrayList<>();
     buildVideoRenderers(context, drmSessionManager, allowedVideoJoiningTimeMs,
         eventHandler, videoRendererEventListener, extensionRendererMode, renderersList);
@@ -282,8 +284,8 @@ public class DefaultRenderersFactory implements RenderersFactory {
    * @param extensionRendererMode The extension renderer mode.
    * @param out An array to which the built renderers should be appended.
    */
-  protected void buildTextRenderers(Context context, TextRenderer.Output output,
-      Looper outputLooper, @ExtensionRendererMode int extensionRendererMode,
+  protected void buildTextRenderers(Context context, TextOutput output, Looper outputLooper,
+      @ExtensionRendererMode int extensionRendererMode,
       ArrayList<Renderer> out) {
     out.add(new TextRenderer(output, outputLooper));
   }
@@ -298,9 +300,8 @@ public class DefaultRenderersFactory implements RenderersFactory {
    * @param extensionRendererMode The extension renderer mode.
    * @param out An array to which the built renderers should be appended.
    */
-  protected void buildMetadataRenderers(Context context, MetadataRenderer.Output output,
-      Looper outputLooper, @ExtensionRendererMode int extensionRendererMode,
-      ArrayList<Renderer> out) {
+  protected void buildMetadataRenderers(Context context, MetadataOutput output, Looper outputLooper,
+      @ExtensionRendererMode int extensionRendererMode, ArrayList<Renderer> out) {
     out.add(new MetadataRenderer(output, outputLooper));
   }
 
