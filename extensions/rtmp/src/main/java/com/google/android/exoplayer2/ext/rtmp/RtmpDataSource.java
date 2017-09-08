e5e32ef602d38276eefdb9e244eec7bdd059a2c7
kharchenkovitaliypt <kharchenkovitaliypt@gmail.com>
2017-09-08 14:13:13 +0300

    Modified rtmp client dependency

--- extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
+++ extensions/rtmp/src/main/java/com/google/android/exoplayer2/ext/rtmp/RtmpDataSource.java
@@ -17,6 +17,8 @@ package com.google.android.exoplayer2.ext.rtmp;
 
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -37,24 +39,30 @@ public final class RtmpDataSource implements DataSource {
 
   @Nullable private final TransferListener<? super RtmpDataSource> listener;
 
+  private final RtmpClient.Config config;
   private RtmpClient rtmpClient;
   private Uri uri;
 
   public RtmpDataSource() {
-    this(null);
+    this(null, null);
   }
 
   /**
    * @param listener An optional listener.
    */
   public RtmpDataSource(@Nullable TransferListener<? super RtmpDataSource> listener) {
+    this(null, listener);
+  }
+
+  public RtmpDataSource(@Nullable RtmpClient.Config config, @Nullable TransferListener<? super RtmpDataSource> listener) {
+    this.config = config;
     this.listener = listener;
   }
 
   @Override
   public long open(DataSpec dataSpec) throws RtmpIOException {
     rtmpClient = new RtmpClient();
-    rtmpClient.open(dataSpec.uri.toString(), false);
+    rtmpClient.open(dataSpec.uri.toString(), config, false);
 
     this.uri = dataSpec.uri;
     if (listener != null) {
@@ -66,6 +74,7 @@ public final class RtmpDataSource implements DataSource {
   @Override
   public int read(byte[] buffer, int offset, int readLength) throws IOException {
     int bytesRead = rtmpClient.read(buffer, offset, readLength);
+    Log.d("TAG", "read() bytesRead: " + bytesRead);
     if (bytesRead == -1) {
       return C.RESULT_END_OF_INPUT;
     }
